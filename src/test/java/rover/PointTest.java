399f1f8eb7cf745c79ba391fcb369ecd4e47e979
sparsick <sparsick@web.de>
2019-11-07 21:22:02 +0100

    SPAR-43: use @MethodSource and incite a naming war

--- src/test/java/rover/PointTest.java
+++ src/test/java/rover/PointTest.java
@@ -2,30 +2,59 @@ package rover;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PointTest {
 
-	Point createPoint(int x, int y, int z) {
-		return new Point(x, y, z);
-	}
+    private static Stream<TestPoint> giveMeSomeValuesDammit() {
+        return Stream.of(
+                new TestPoint(0, 0, 0),
+                new TestPoint(1, 0, 0),
+                new TestPoint(0, 2, 0),
+                new TestPoint(0, 2, 5));
+    }
 
-	@ParameterizedTest
-	@CsvSource({"0, 0, 0", "1, 0, 0", "0, 2, 0", "0, 2, 5"})
-	void testPoints(int x, int y, int z) {
-		assertThat(createPoint(x, y, z))
-				.hasFieldOrPropertyWithValue("x", x)
-				.hasFieldOrPropertyWithValue("y", y)
-				.hasFieldOrPropertyWithValue("z", z);
-	}
+    Point createPoint(int x, int y, int z) {
+        return new Point(x, y, z);
+    }
 
-	@Test
-	void testEqual() {
-		Point point1 = createPoint(0, 2, 5);
-		Point point2 = createPoint(0, 2, 5);
-		assertTrue(point1.equals(point2));
-	}
+    @ParameterizedTest
+    @MethodSource("giveMeSomeValuesDammit")
+    void testPoints(TestPoint tp) {
+        assertThat(createPoint(tp.x, tp.y, tp.z))
+                .hasFieldOrPropertyWithValue("x", tp.x)
+                .hasFieldOrPropertyWithValue("y", tp.y)
+                .hasFieldOrPropertyWithValue("z", tp.z);
+    }
+
+    @Test
+    void testEqual() {
+        Point point1 = createPoint(0, 2, 5);
+        Point point2 = createPoint(0, 2, 5);
+        assertTrue(point1.equals(point2));
+    }
+
+    private static class TestPoint {
+        public int x, y, z;
+
+        public TestPoint(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        @Override
+        public String toString() {
+            return "TestPoint{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    ", z=" + z +
+                    '}';
+        }
+    }
 }
