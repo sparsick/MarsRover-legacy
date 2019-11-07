40a237a033da5d72503a7c6f4baf58abee370615
sparsick <sparsick@web.de>
2019-11-07 21:11:31 +0100

    SPAR-42: use parameterized tests - because we can
    Sandra forced us to do it.

--- src/test/java/rover/PointTest.java
+++ src/test/java/rover/PointTest.java
@@ -1,56 +1,31 @@
 package rover;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PointTest {
+class PointTest {
 
-	public Point createPoint(int x, int y, int z) {
-		Point point = new Point(x, y, z);
-		return point;
+	Point createPoint(int x, int y, int z) {
+		return new Point(x, y, z);
+	}
+
+	@ParameterizedTest
+	@CsvSource({"0, 0, 0", "1, 0, 0", "0, 2, 0", "0, 2, 5"})
+	void testPoints(int x, int y, int z) {
+		assertThat(createPoint(x, y, z))
+				.hasFieldOrPropertyWithValue("x", x)
+				.hasFieldOrPropertyWithValue("y", y)
+				.hasFieldOrPropertyWithValue("z", z);
 	}
 
 	@Test
-	public void testgetXCoordinate() {
-		Point point = createPoint(0, 0, 0);	
-		assertEquals(0,point.x);
-	}
-
-	@Test
-	public void testgetYCoordinate() {
-		Point point = createPoint(0, 0, 0);	
-		assertEquals(0,point.y);
-	}
-
-	@Test
-	public void testgetZCoordinate() {
-		Point point = createPoint(0, 0, 0);	
-		assertEquals(0,point.z);
-	}
-
-	@Test
-	public void testgetDifferentXCoordinate() {
-		Point point = createPoint(1, 0, 0);	
-		assertEquals(1,point.x);
-	}
-
-	@Test
-	public void testgetDifferentYCoordinate() {
-		Point point = createPoint(0, 2, 0);	
-		assertEquals(2,point.y);
-	}
-
-	@Test
-	public void testgetDifferentZCoordinate() {
-		Point point = createPoint(0, 2, 5);	
-		assertEquals(5,point.z);
-	}
-	
-	@Test
-	public void testEqual() {
-		Point point1 = createPoint(0, 2, 5);	
-		Point point2 = createPoint(0, 2, 5);	
+	void testEqual() {
+		Point point1 = createPoint(0, 2, 5);
+		Point point2 = createPoint(0, 2, 5);
 		assertTrue(point1.equals(point2));
 	}
 }
